a89b1145e02de036512c619a32dc3bff235c9d1f
lexirohrer <53787944+lexirohrer@users.noreply.github.com>
2025-09-14 10:58:54 +0200

    Fix base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/p-950922/' : '/',
+  base: mode === 'production' ? '/uxlex-portfolio/' : '/',
   server: {
     host: "::",
     port: 8080,
